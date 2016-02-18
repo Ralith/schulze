8660dfb0aad4c210d6eb29f37fade5012e97e4ee
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-02-17 23:32:12 -0800

    Functional scraper

--- scraper/Main.hs
+++ scraper/Main.hs
@@ -6,17 +6,18 @@ import Data.Foldable
 import Data.List.Split
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (isJust, listToMaybe)
+import Data.Maybe (isJust)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Encoding.Error (lenientDecode)
 import qualified Data.Text.IO as T
-import Data.Text.Lazy.Encoding (decodeUtf8With)
+import  Data.Text.Lazy.Encoding (decodeUtf8With)
 import qualified Data.Text.Read as T
 import Data.Word (Word64)
-import Debug.Trace
+import Data.Either
 
 import Network.HTTP.Client
+import Network.HTTP.Types.Status
 import Network.URI
 
 import System.Environment
@@ -24,28 +25,27 @@ import System.Exit
 import System.IO
 
 import Text.HTML.TagSoup
-import Text.HTML.TagSoup.Tree
-
-instance Plated (TagTree a) where
-  plate _ t@(TagLeaf _) = pure t
-  plate f (TagBranch n attrs xs) = TagBranch n attrs <$> traverse f xs
 
 newtype Thread = Thread { _thread :: Word64 }
   deriving (Eq, Show)
 makePrisms ''Thread
 
 newtype Post = Post { _post :: Word64 }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 makePrisms ''Post
 
-data PostData = PostData { _postAuthor :: Text, _postBody :: [TagTree Text] }
+data PostData a = PostData { _postAuthor :: Text, _postBody :: a }
 makeLenses ''PostData
 
 data ThreadPage = ThreadPage { _tpThread :: Thread, _tpPerPage :: Word, _tpPage :: Word }
+  deriving Eq
 makeLenses ''ThreadPage
 
 instance Show ThreadPage where
   show = tpUrl
+
+usage :: String
+usage = "<start url> [end url]"
 
 main :: IO ()
 main = do
@@ -56,67 +56,117 @@ main = do
     [startURI] ->
       case parsePostURI startURI of
         Left err -> hPutStr stderr "not a valid post URL: " >> hPutStrLn stderr err >>  printUsage >> exitFailure
-        Right (tp, p) -> do
-          posts <- getPosts tp p Nothing
-          T.putStrLn . T.concat $ map (\(post, x) ->
-                                        T.concat [ x ^. postAuthor
-                                                 , " (post ", T.pack $ show (post ^. _Post), ")\n"
-                                                 -- , T.pack . show $ x ^. postBody
-                                                 -- , "\n\n"
-                                                 ]) posts
+        Right start -> process =<< getPosts start Nothing
+    [startURI, endURI] ->
+      case (parsePostURI startURI, parsePostURI endURI) of
+        (Left err, _) -> do
+          hPutStr stderr "start URL not a valid post URL: "
+          hPutStrLn stderr err
+          printUsage
+          exitFailure
+        (Right _, Left err) -> do
+          hPutStr stderr "end URL not a valid post URL: "
+          hPutStrLn stderr err
+          printUsage
+          exitFailure
+        (Right start, Right end) -> process =<< getPosts start (Just end)
     _ -> printUsage >> exitFailure
 
-usage :: String
-usage = "<start url> [end url]"
+process :: Either Status [(Post, PostData [Tag Text])] -> IO ()
+process (Right posts) = do
+  putStr $ show (length posts) ++ " posts, "
+  let votes = map (getVote . (postBody %~ bolded) . snd) posts ^.. folded._Just
+  putStrLn $ show (length votes) ++ " votes"
+  T.putStrLn . T.concat $ map (\x ->
+                                 T.concat [ x ^. postAuthor, "\n"
+                                          , x ^. postBody, "\n\n"
+                                          ]) votes
+process (Left err) = do
+  hPutStr stderr $ "server returned HTTP " ++ show (statusCode err) ++ ": "
+  T.hPutStrLn stderr $ (decodeUtf8With lenientDecode $ statusMessage err ^. from strict) ^. strict
+  exitFailure
 
-getPosts :: ThreadPage -> Post -> Maybe Post -> IO [(Post, PostData)]
-getPosts startPage startPost endPost = do
+bolded :: [Tag Text] -> Text
+bolded = T.strip . go 0
+  where
+    go :: Word -> [Tag Text] -> Text
+    go n (TagOpen "b" _ : xs) = go (succ n) xs
+    go n (TagClose "b" : xs) = go (pred n) xs
+    go 0 (_:xs) = go 0 xs
+    go n (TagText t : xs) = T.append t (go n xs)
+    go n (_ : xs) = go n xs
+    go _  [] = ""
+
+getVote :: PostData Text -> Maybe (PostData Text)
+getVote post =
+  case voteLines of
+    [] -> Nothing
+    _ -> Just $ set postBody (T.intercalate "\n" (drop 1 voteLines)) post
+  where
+    flag = T.toCaseFold "##vote"
+    voteLines :: [Text]
+    voteLines = takeWhile (not . T.null) . dropWhile (not . (== flag) . T.toCaseFold) . map T.strip . T.lines $ post ^. postBody
+
+getPosts :: (ThreadPage, Post) -> Maybe (ThreadPage, Post) -> IO (Either Status [(Post, PostData [Tag Text])])
+getPosts start end = do
   mgr <- newManager defaultManagerSettings
-  -- tail . dropWhile ((/= startPost) . fst) <$> 
-  ps <- getPosts' mgr startPage endPost
-  putStrLn $ show (length ps) ++ " posts"
+  ps <- fmap (filter (\(p, _) -> p > (start ^. _2) && maybe True ((p <=) . (^._2)) end))
+          <$> getPosts' mgr (start ^. _1) ((^. _1) <$> end)
   pure ps
 
-hasClass :: Text -> [Attribute Text] -> Bool
-hasClass c t = isJust (join $ find (==c) . T.split (==' ') <$> lookup "class" t)
+tagAttribs :: Tag a -> [Attribute a]
+tagAttribs (TagOpen _ xs) = xs
+tagAttribs _ = []
 
--- 455143180 20773
-getPosts' :: Manager -> ThreadPage -> Maybe Post -> IO [(Post, PostData)]
-getPosts' mgr currentPage endPost = do
+hasClass :: Text -> Tag Text -> Bool
+hasClass c (TagOpen _ attribs) = isJust (join $ find (==c) . T.split (==' ') <$> lookup "class" attribs)
+hasClass _ _ = False
+
+hasClass' :: Text -> [Attribute Text] -> Bool
+hasClass' c t = isJust (join $ find (==c) . T.split (==' ') <$> lookup "class" t)
+
+isPost :: Tag Text -> Bool
+isPost tag = hasClass "post" tag && fromAttrib "id" tag /= "post"
+
+getPosts' :: Manager -> ThreadPage -> Maybe ThreadPage -> IO (Either Status [(Post, PostData [Tag Text])])
+getPosts' mgr currentPage endPage = do
   page <- getPage mgr currentPage
-  putStrLn $ show (length (filter (\case {TagBranch _ attrs _ -> hasClass "post" attrs; _ -> False}) page)) ++ ""
-  let isPost :: (Text, [Attribute Text], a) -> Bool
-      isPost (n, attrs, _) = n == "table" && hasClass "post" attrs && lookup "id" attrs /= Just "post"
-      getId (_, attrs, body) = do
-        str <- maybe (Left "post missing id attribute") Right (lookup "id" attrs)
-        idx <- maybe (Left "post missing data-idx attribute") Right (lookup "data-idx" attrs)
-        pid <- Post <$> decimal (T.unpack . T.drop 4 $ (trace (show idx) str))
-        pure (pid, body)
-      branches :: [(Text, [Attribute Text], [TagTree Text])]
-      branches = [(a, b, c) | TagBranch a b c <- concatMap universe page]
-      posts = map (_Right._2 %~ parsePost) . map getId . filter isPost $ branches
-  foldrM (\x xs ->
-            case x of
-              Left err -> hPutStrLn stderr ("malformed post id: " ++ err) >> pure xs
-              Right (Post pid, Left err) -> hPutStrLn stderr ("malformed user info for post " ++ show pid ++ ": " ++ err) >> pure xs
-              Right (pid, Right post) -> pure $ (pid, post) : xs
-         ) [] posts
+  putStrLn $ "got page " ++ show (currentPage ^. tpPage)
+  case page of
+    Left err -> pure (Left err)
+    Right tags -> do
+      let posts = map parsePost . partitions isPost $ tags
+      forM_ (lefts posts) $ \err -> hPutStrLn stderr $ "parse failure: " ++ err
+      let posts' = rights posts
+      if Just currentPage == endPage || fromIntegral (length posts) < (currentPage ^. tpPerPage)
+         then pure (Right posts')
+         else (fmap (posts' ++)) <$> getPosts' mgr (currentPage & tpPage %~ succ) endPage
 
-parsePost :: [TagTree Text] -> Either String PostData
-parsePost tree = PostData <$> author <*> body
-  where
-    author = maybe (Left "couldn't find author") Right $ listToMaybe
-               [t | (TagBranch _ attrs [TagLeaf (TagText t)]) <- concatMap universe tree, hasClass "author" attrs]
-    body = maybe (Left "couldn't find body") Right $ listToMaybe
-             [b | (TagBranch _ attrs b) <- concatMap universe tree, hasClass "postbody" attrs]
+parsePost :: [Tag Text] -> Either String (Post, PostData [Tag Text])
+parsePost [] = error "parsePost: impossible" -- partitions guarantees nonempty
+parsePost (x:xs) = do
+  pidStr <- maybe (Left "post missing id attribute") Right $ lookup "id" (tagAttribs x)
+  pid <- decimal (T.unpack . T.drop 4 $ pidStr)
+  (author, rest) <-
+    case dropWhile (not . hasClass "author") xs of
+      (_:TagText author:rest) -> Right (author, rest)
+      _ -> Left "post missing author"
+  case dropWhile (/= TagComment " google_ad_section_start ") rest of
+    (_:body) -> Right (Post pid, PostData author (takeWhile (/= TagComment " google_ad_section_end ") body))
+    _ -> Left $ "post " ++ show pid ++ " missing body"
 
 matches :: [a -> Bool] -> a -> Bool
 matches fs x = all ($ x) fs
 
-getPage :: Manager -> ThreadPage -> IO [TagTree Text]
+getPage :: Manager -> ThreadPage -> IO (Either Status [Tag Text])
 getPage mgr tp = do
-  response <- flip httpLbs mgr =<< parseUrl (tpUrl tp)
-  pure $ (map (fmap (^. strict)) . parseTree . decodeUtf8With lenientDecode . responseBody) response
+  req <- parseUrl (tpUrl tp)
+  let req' = req { checkStatus = (\_ _ _ -> Nothing)}
+  response <- flip httpLbs mgr req'
+  pure $ if statusIsSuccessful (responseStatus response)
+            then Right $ (map (fmap (^. strict)) . canonicalizeTags . parseTags . decodeUtf8With lenientDecode . responseBody)
+                           response
+            else Left (responseStatus response)
 
 -- http://forums.somethingawful.com/showthread.php?threadid=3657951&userid=0&perpage=40&pagenumber=520#post455115330
 parsePostURI :: String -> Either String (ThreadPage, Post)
@@ -126,11 +176,11 @@ parsePostURI u =
     Just u' ->
       let opts = queryOpts (uriQuery u') in
       case ( decimal <$> M.lookup "threadid" opts
-           , decimal <$> M.lookup "perpage" opts
+           , maybe (Right 40) decimal $ M.lookup "perpage" opts
            , decimal <$> M.lookup "pagenumber" opts
            , _2 %~ decimal $ splitAt 5 (uriFragment u')
            ) of
-        (Just (Right tid), Just (Right pp), Just (Right page), ("#post", Right pid)) ->
+        (Just (Right tid), Right pp, Just (Right page), ("#post", Right pid)) ->
           Right (ThreadPage (Thread tid) pp page, Post pid)
         _ -> Left "missing or malformed parameters"
 
